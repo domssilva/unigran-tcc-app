09a7aa1f416c2248c95f7b4ab478e0d6cf8c29a5
domssilva <domenique_silva@hotmail.com>
2025-05-01 12:06:52 +0200

    feature: created flask main file

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Initialize Flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///app.db')
+DEBUG = os.getenv('DEBUG')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# Routes
+@app.route('/')
+def home():
+    return "Hello Flask"
+
+if __name__ == '__main__':
+    app.run(debug=os.getenv('DEBUG', DEBUG))
