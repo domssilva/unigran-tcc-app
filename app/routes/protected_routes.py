080b27b7a54a7d6f632edea112f03be60853378b
domssilva <domenique_silva@hotmail.com>
2025-06-03 23:28:55 +0200

    feature: edit vulnerability

--- app/routes/protected_routes.py
+++ app/routes/protected_routes.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from models.application import Application
 from models.vulnerability import Vulnerability
 from services.application_service import create_application
-from services.vulnerability_service import create_vulnerability
+from services.vulnerability_service import create_vulnerability, update_vulnerability
 
 protected_bp = Blueprint('protected', __name__)
 
@@ -49,14 +49,24 @@ def app_detail(id):
 @login_required
 def register_vulnerability(app_id):
     app = Application.query.filter_by(id=app_id, user_id=current_user.id).first_or_404()
+    if request.method == 'POST':
+        description = request.form['description']
+        severity = request.form.get('severity', 'média')
+        status = request.form.get('status', 'aberto')
+        create_vulnerability(description, severity, status, app)
+        return redirect(url_for('protected.app_detail', id=app.id))
+    return render_template('vulnerability_register.html', app=app)
+
+@protected_bp.route('/app/<int:app_id>/vuln/<int:vuln_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_vulnerability(app_id, vuln_id):
+    app = Application.query.filter_by(id=app_id, user_id=current_user.id).first_or_404()
+    vuln = Vulnerability.query.filter_by(id=vuln_id, application_id=app.id).first_or_404()
 
     if request.method == 'POST':
         description = request.form['description']
         severity = request.form.get('severity', 'média')
         status = request.form.get('status', 'aberto')
-
-        create_vulnerability(description, severity, status, app)
-
+        update_vulnerability(vuln, description, severity, status)
         return redirect(url_for('protected.app_detail', id=app.id))
-
-    return render_template('vulnerability_register.html', app=app)
+    return render_template('vulnerability_edit.html', app=app, vuln=vuln)
